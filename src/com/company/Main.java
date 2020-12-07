e70b87a5dc7fd16e2cb8c4d41efd38a9bc2949bc
SebastianMarie <sebastian.rgs@hotmail.com>
2020-12-08 00:14:28 +0200

    asd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,21 @@ public class Main {
         System.out.println(22/3);
         System.out.println(20+-3*518);
         System.out.println(5+15/3*2-8%3);
+int a = 8;
+int b = 3;
+int c = a + b;
+        System.out.println("Addition result = " + c);
+        int d = 22;
+        int e = 7;
+        int f = d / e;
+        System.out.println("Subtraction result = " + f);
+        int g = -5;
+        int h = 8;
+        int i = 6;
+        int j = h * i;
+        int k = g + j;
+        System.out.println("Multiplying result = " +j);
+        System.out.println("Addition result = " +k);
 
 
 
