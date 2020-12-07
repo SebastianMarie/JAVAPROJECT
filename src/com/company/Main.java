dbc084515b776f5609d74a7ef70ac388a1d0fffe
SebastianMarie <sebastian.rgs@hotmail.com>
2020-12-07 23:11:34 +0200

    asd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,20 @@ public class Main {
 	// write your code here
 
         System.out.println("Hello Sebi");
+        System.out.println(8+3);
+        System.out.println(22/3);
+        System.out.println(20+-3*518);
+        System.out.println(5+15/3*2-8%3);
+
+
+
+
+
+
+
+
+
+
+
     }
 }
