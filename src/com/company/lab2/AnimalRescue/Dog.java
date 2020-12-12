3267abc095cb10f868d4d677599259938b05e0db
SebastianMarie <sebastian.rgs@hotmail.com>
2020-12-12 21:25:35 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/com/company/lab2/AnimalRescue/Dog.java
@@ -0,0 +1,39 @@
+package com.company.lab2.AnimalRescue;
+
+public class Dog  {
+        String name = "Ami";
+        double ageInYears = 0.5;
+        double weightInKilograms = 2.5;
+        String color = "white";
+        double heightInMeters = 0.3;
+        String race = "Bichon";
+        void bark() {
+            System.out.println("Woof!");
+        }
+        void eat() {
+            System.out.println("eating dogfood!");
+        }
+        void sleep() {
+            System.out.println("Zzzz...*snore");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/com/company/lab2/PrintSigns/AppMain/PrintSigns.java
@@ -0,0 +1,27 @@
+package com.company.lab2.PrintSigns.AppMain;
+
+public class PrintSigns {
+    public static void main(String[] args) {
+        String patern1 = "   j   a   v     v   a   ";
+        String patern2 = "   j  a a   v   v   a a  ";
+        String patern3 = "j  j aaaaa   v v   aaaaa ";
+        String patern4 = " jj a     a   v   a     a";
+        System.out.println(patern1);
+        System.out.println(patern2);
+        System.out.println(patern3);
+        System.out.println(patern4);
+
+        String patern5 = "         ";
+        String patern6 = " +-----+ ";
+        String patern7 = "(| o o |)";
+        String patern8 = " |  ^  | ";
+        String patern9 = " | `-` | ";
+        String patern10 = " +-----+ ";
+        System.out.println(patern5);
+        System.out.println(patern6);
+        System.out.println(patern7);
+        System.out.println(patern8);
+        System.out.println(patern9);
+        System.out.println(patern10);
+    }
+}
