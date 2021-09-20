1d8bfb9b4cd0431f632f8773ae506bf2c64424a1
SebastianMarie <sebastian.rgs@hotmail.com>
2021-09-20 22:56:44 +0300

    Merge remote-tracking branch 'origin/master'

--- src/com/company/lab6/Logic.java
+++ src/com/company/lab6/Logic.java
@@ -3,7 +3,10 @@ package com.company.lab6;
 import java.util.List;
 
 public class Logic {
-    //TEMA
+
+
+    //TEMA LISTS
+
 //1. Scrieti o metoda Java, care sa primeasca parametru o Lista, si sa afiseze, pe rand, toate
 // valorile din lista, fiecare pe rand nou.
     public void printList(List<Integer> myList) {
